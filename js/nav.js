f6ca48d8204ba52d8b7c14208822359bdd372c55
davidjeffers <ohsjeffers@gmail.com>
2022-03-17 14:24:23 -0500

    first

---
+++ js/nav.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const $submitNav = $("#nav-submit-story");
+const $submitForm = $("#submit-form");
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  putStoriesOnPage();
+}
+// FIXME: do we need to include the child selector?
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+// user clicks submit on Navbar
+// onclick hidePageComponents from main.js
+// new story form unhidden
+function navNewStoryForm() {
+  hidePageComponents();
+  $submitForm.show();
+}
+$submitNav.on("click", console.log("submit"));
